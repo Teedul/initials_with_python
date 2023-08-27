cbc0f94dd69d49ee72c17835155923edf129faef
Teedul <104795464+Teedul@users.noreply.github.com>
2023-08-27 14:49:38 -0500

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,10 @@
+# First & last name: Tony Davis 
+# Fun fact: I lived in Okinawa Japan
+
+print("TTTTT DDDD ")
+print("  T  ", "D   D")
+print("  T  ", "D   D")
+print("  T  ", "D   D")
+print("  T  ", "D   D")
+print("  T  ", "D   D")
+print("  T  ", "DDDD")
